a0bde188292457c1e346985ff0918c9f12a0f9dd
Thomas Prost <thomasprost38@gmail.com>
2018-02-28 17:54:37 +0900

    fixes many bugs on Recipe crawl. Better error handling. Now checks if the same link was used to create a recipe recently

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -30,35 +30,58 @@ class DefaultController extends Controller
         ));
     }
     /**
-     * @Route("/parse-recipe", name="parseRecipe")
+     * @Route("/parse-recipe", name="parse_recipe")
      */
     public function parseRecipeUrl(Request $request, ValidatorInterface $validator){
 
-        $crawlHelper = new CrawlHelper('https://cookpad.com/recipe/1203423');
-
-        $html = $crawlHelper->getHtml();
         $errorsString = null;
 
-//        // Get the Html from a cUrl request
-//        $html = $this->dlPage('https://cookpad.com/recipe/1203423');
-        // Crawl the html to create a new recipe
-        if($html){
-            $recipe = $crawlHelper->crawlHtml($html);
+        // TODO : better error handling and message return
+        // TODO : move code in the Crawl Helper to keep Controller as small as possible
 
-            $errors = $validator->validate($recipe);
-            if (count($errors) > 0) {
-                $errorsString = "Sorry an error occurred, we couldn't process this link";
+        if ('POST' === $request->getMethod()) {
+            $urlField = $request->request->get('url');
+            if($urlField !== ''){
+                $repository = $this->getDoctrine()->getRepository(Recipe::class);
+                $hasRecipe = $repository->recipeJustCreated($urlField);
+
+                if(empty($hasRecipe)){
+                    // Create new instance of the helper
+                    $crawlHelper = new CrawlHelper($urlField);
+
+                    // Get the Html from the cUrl request
+                    $html = $crawlHelper->getHtml();
+//                $html = null;
+
+                    // Crawl the html to create a new recipe
+                    if($html){
+                        $recipe = $crawlHelper->crawlHtml($html);
+
+                        $errors = $validator->validate($recipe);
+                        if (count($errors) > 0) {
+                            $errorsString = "Sorry an error occurred, we couldn't process this link";
+                        }
+                        else{
+                            $em = $this->getDoctrine()->getManager();
+                            $em->persist($recipe);
+                            $em->flush();
+
+                            return $this->redirectToRoute('recipe_edit', ['slug'=>$recipe->getSlug()]);
+                        }
+
+                    }else{
+                        $errorsString = "Sorry, no recipe could be found for this url";
+                    }
+                }else{
+                    $errorsString = "You have just created a recipe from this link";
+                }
+
             }
             else{
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($recipe);
-                $em->flush();
-
-                return $this->redirectToRoute('recipe_edit', ['slug'=>$recipe->getSlug()]);
+                $errorsString = "Please enter an url before clicking the GENERATE button.";
             }
 
         }
-
 
 
         return $this->render('default/parseRecipe.html.twig', array(
